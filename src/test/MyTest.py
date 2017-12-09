099885c346fa80079c23f96557f7675c4b6d6d90
jessie517 <jessie_517@126.com>
2017-12-09 11:32:34 +0800

    Initial commit

---
+++ src/test/MyTest.py
@@ -0,0 +1,12 @@
+'''
+Created on 2017年12月9日
+
+@author: chenjiaxu
+'''
+import network
+import mnist_loader
+
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+
+net = network.Network([784, 30, 10])
+net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
